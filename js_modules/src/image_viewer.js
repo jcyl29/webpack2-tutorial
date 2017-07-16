ace8cd80732631eb26eeea5df934787f05218c53
Justin Lui <jlui@surveymonkey.com>
2017-07-16 01:39:54 -0700

    code splitting and System.import

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,14 +1,13 @@
 // https://github.com/tcoopman/image-webpack-loader/issues/51
 // need `brew install libpng` to run 'image-webpack-loader' on local machine
-import big from '../assets/big.jpg'
 import small from '../assets/small.jpg'
 import '../styles/image_viewer.css'
 
-const image = document.createElement('img');
-image.src = small;
+// wrapping this code in a arrow function is NOT a requirement
+// for code splitting
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
 
-document.body.appendChild(image);
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-document.body.appendChild(bigImage);
+    document.body.appendChild(image);
+}
